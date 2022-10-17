821dfc4dea81786461ad89b2bfcf026be8c33862
OriginalMeet <ravalm320@gmail.com>
2022-10-16 23:24:19 -0400

    testing tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          JobHunt for Unit-604 will be live on: Oct 17
-        </div>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 

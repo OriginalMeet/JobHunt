fa8681b37b33378e4831f370370f8b2d29eb8a3c
OriginalMeet <ravalm320@gmail.com>
2022-10-22 18:36:55 -0400

    Adding the day page

--- src/pages/Day/Index.tsx
+++ src/pages/Day/Index.tsx
@@ -1,8 +1,22 @@
+import { useLocation } from 'react-router-dom';
+import QuotesBG from '../../assets/QuotesBG.png';
+
 
 export default function Day() {
+    const location = useLocation();
+    const day = location.state.id;
+
     return (
         <div>
-            Day Info page
+            <img className="absolute h-full w-full object-cover -z-10" src={QuotesBG} alt="Background img"/>
+            <div className="h-screen flex flex-col justify-between">
+                <div className="flex justify-center font-heading text-xl mt-2">
+                    Job Hunt
+                </div>
+                <div className="flex justify-center w-5/6 h-5/6 m-aut shadow-2xl">
+                    Day Page: {day}
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Days/DayButtons.tsx
+++ src/pages/Days/DayButtons.tsx
@@ -6,7 +6,7 @@ export default function DayButtons() {
     return (
         <div className="flex flex-row flex-wrap justify-around p-12 w-full h-full">
             {[...Array(30)].map((_, i) =>
-                <button onClick={() => { navigate("/day"); }} key={i+1} type="button" className="disabled transition-all hover:transition-all flex w-24 h-24 m-4 px-8 py-4 justify-center font-poiret font-black text-xl text-violet-900 bg-purple-100 rounded-md shadow hover:bg-purple-200 hover:rounded-3xl hover:text-2xl">
+                <button onClick={() => { navigate("/day", {state:{id:i+1}}); }} key={i+1} type="button" className="disabled transition-all hover:transition-all flex w-24 h-24 m-4 px-8 py-4 justify-center font-poiret font-black text-xl text-violet-900 bg-purple-100 rounded-md shadow hover:bg-purple-200 hover:rounded-3xl hover:text-2xl">
                     Day {i+1}
                 </button>
             )}
